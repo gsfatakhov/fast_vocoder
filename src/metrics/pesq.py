37a2e5fbb922097364dead633121395e711dd4d5
Georgii Fatakhov <fatahoff.georgy@yandex.ru>
2025-04-22 15:38:06 +0300

    pesq add resample, lv loss add norm in fm_loss

--- src/metrics/pesq.py
+++ src/metrics/pesq.py
@@ -1,16 +1,26 @@
 import torch
-from sympy import preorder_traversal
 
 from src.metrics.base_metric import BaseMetric
 from torchmetrics.audio.pesq import PerceptualEvaluationSpeechQuality
+from pesq import NoUtterancesError
+from torchaudio.transforms import Resample
 
 
 class PESQ(BaseMetric):
-    # Не поддерживает fs=22050
-    def __init__(self, device="auto", fs=16000, mode="wb", n_processes=1, *args, **kwargs):
+    def __init__(self, device="auto", fs=22050, mode="wb", n_processes=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if device == "auto":
             device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.orig_fs = fs
+        self.need_resample = False
+        if self.orig_fs > 16000:
+            self.resampler = Resample(self.orig_fs, 16000)
+            self.need_resample = True
+        elif self.orig_fs != 8000:
+            self.resampler = Resample(self.orig_fs, 8000)
+            self.need_resample = True
+
         self.metric = PerceptualEvaluationSpeechQuality(fs, mode=mode, n_processes=n_processes).to(device)
 
     def __call__(
@@ -19,4 +29,11 @@ class PESQ(BaseMetric):
             pred_audio: torch.Tensor,
             **kwargs
     ):
-        return self.metric(pred_audio, audio)
+        if self.need_resample:
+            audio = self.resampler
+        val = 0.0
+        try:
+            val = self.metric(pred_audio, audio)
+        except NoUtterancesError:
+            pass
+        return val
