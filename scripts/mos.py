8d627ba2df52f607ff24a8e9a02f89d87a29aacf
gsfatakhov <fatahoff.georgy@yandex.ru>
2025-05-01 20:53:43 +0300

    mos fix

--- scripts/mos.py
+++ scripts/mos.py
@@ -1,9 +1,9 @@
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from torch.serialization import add_safe_globals
 from wvmos import get_wvmos
 
-
-add_safe_globals(['pytorch_lightning.callbacks.model_checkpoint.ModelCheckpoint'])
+add_safe_globals([ModelCheckpoint])
 
 model = get_wvmos()
-mos = model.calculate_dir("/Users/georgijfatahov/PycharmProjects/fast_vocoder/resynthesize_results", mean=True)
+mos = model.calculate_dir("/home/georgii/PycharmProjects/fast_vocoder/datasets/5_ljspeech_dataset/wavs", mean=True)
 print(mos)
