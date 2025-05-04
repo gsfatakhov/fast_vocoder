8aafb63f06eb38fd232dfd89da47ce31be0758cc
Georgii Fatakhov <fatahoff.georgy@yandex.ru>
2025-05-04 21:08:05 +0300

    mambavoc model with approximate params, fix istft from lightvoc_mods

---
+++ src/model/mambavoc.py
@@ -0,0 +1,74 @@
+from src.model.src_mambavoc.models import Generator, CoMBD, SBD, MultiResSpecDiscriminator
+from src.utils.mel import MelSpectrogram
+from src.model.src_lightvoc.pqmf import PQMF
+
+from src.model.gan_base_model import GanBaseModel
+
+import torch.nn as nn
+
+
+class DiscriminatorModel(nn.Module):
+    def __init__(self, pqmf_config, combd_params, sbd_params):
+        super().__init__()
+
+        subbands2, taps2, cutoff_ratio2, beta2 = pqmf_config["lv2"]
+        # in reference repo using "lv2""
+        subbands1, taps1, cutoff_ratio1, beta1 = pqmf_config["lv1"]
+
+        pqmf_lv2 = PQMF(subbands2, taps2, cutoff_ratio2, beta2)
+        pqmf_lv1 = PQMF(subbands1, taps1, cutoff_ratio1, beta1)
+
+        self.combd = CoMBD(pqmf_list=[pqmf_lv2, pqmf_lv1], **combd_params)
+        self.sbd = SBD(**sbd_params)
+        self.msd = MultiResSpecDiscriminator()
+
+    def forward(self, real_audio, generated_audio):
+        # audio: [B, 1, T]
+
+        # if use this, need to add some conv in generator, follow: https://github.com/ncsoft/avocodo/blob/2999557bbd040a6f3eb6f7006a317d89537b78cd/avocodo/models/generator.py#L109
+        # in this implement, only last conv is used
+
+        # ys = [
+        #     pqmf_lv2.analysis(
+        #         y
+        #     )[:, :h.projection_filters[1]],
+        #     pqmf_lv1.analysis(
+        #         y
+        #     )[:, :h.projection_filters[2]],
+        #     y
+        # ]
+        ys = [real_audio]
+        ys_hat = [generated_audio]
+
+        y_du_hat_r, y_du_hat_g, fmap_u_r, fmap_u_g = self.combd(ys, ys_hat)
+        y_dp_hat_r, y_dp_hat_g, fmap_p_r, fmap_p_g = self.sbd(real_audio, generated_audio)
+        y_ds_hat_r, y_ds_hat_g, fmap_s_r, fmap_s_g = self.msd(real_audio, generated_audio)
+
+        return {
+            "CoMBD": {"y_du_hat_r": y_du_hat_r, "y_du_hat_g": y_du_hat_g, "fmap_u_r": fmap_u_r,
+                      "fmap_u_g": fmap_u_g},
+            "SBD": {"y_dp_hat_r": y_dp_hat_r, "y_dp_hat_g": y_dp_hat_g, "fmap_p_r": fmap_p_r,
+                    "fmap_p_g": fmap_p_g},
+            "MSD": {"y_ds_hat_r": y_ds_hat_r, "y_ds_hat_g": y_ds_hat_g, "fmap_s_r": fmap_s_r,
+                    "fmap_s_g": fmap_s_g},
+        }
+
+
+class MambaVoc(GanBaseModel):
+    def __init__(self, generator_params, discriminator_params, mel_config):
+        generator = Generator(**generator_params)
+        discriminator = DiscriminatorModel(**discriminator_params)
+
+        super().__init__(generator, discriminator)
+
+        self.mel_extractor = MelSpectrogram(mel_config)
+
+    def forward(self, **batch):
+        x = batch["mel"]
+        length = batch["length"]
+
+        spec, phase = self.generator(x, length)
+
+        batch["pred_audio"] = self.mel_extractor.inverse(spec, phase).unsqueeze(1)
+
+        return batch
--- src/utils/mel.py
+++ src/utils/mel.py
@@ -10,6 +10,78 @@ import librosa
 import torch.nn.functional as F
 
 
+class ISTFT(nn.Module):
+    """
+    Custom implementation of ISTFT since torch.istft doesn't allow custom padding (other than `center=True`) with
+    windowing. This is because the NOLA (Nonzero Overlap Add) check fails at the edges.
+    See issue: https://github.com/pytorch/pytorch/issues/62323
+    Specifically, in the context of neural vocoding we are interested in "same" padding analogous to CNNs.
+    The NOLA constraint is met as we trim padded samples anyway.
+
+    Args:
+        n_fft (int): Size of Fourier transform.
+        hop_length (int): The distance between neighboring sliding window frames.
+        win_length (int): The size of window frame and STFT filter.
+        padding (str, optional): Type of padding. Options are "center" or "same". Defaults to "same".
+    """
+
+    def __init__(self, n_fft: int, hop_length: int, win_length: int, padding: str = "same"):
+        super().__init__()
+        if padding not in ["center", "same"]:
+            raise ValueError("Padding must be 'center' or 'same'.")
+        self.padding = padding
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.win_length = win_length
+        window = torch.hann_window(win_length)
+        self.register_buffer("window", window)
+
+    def forward(self, spec: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the Inverse Short Time Fourier Transform (ISTFT) of a complex spectrogram.
+
+        Args:
+            spec (Tensor): Input complex spectrogram of shape (B, N, T), where B is the batch size,
+                            N is the number of frequency bins, and T is the number of time frames.
+
+        Returns:
+            Tensor: Reconstructed time-domain signal of shape (B, L), where L is the length of the output signal.
+        """
+        if self.padding == "center":
+            # Fallback to pytorch native implementation
+            return torch.istft(spec, self.n_fft, self.hop_length, self.win_length, self.window, center=True)
+        elif self.padding == "same":
+            pad = (self.win_length - self.hop_length) // 2
+        else:
+            raise ValueError("Padding must be 'center' or 'same'.")
+
+        assert spec.dim() == 3, "Expected a 3D tensor as input"
+        B, N, T = spec.shape
+
+        # Inverse FFT
+        ifft = torch.fft.irfft(spec, self.n_fft, dim=1, norm="backward")
+        ifft = ifft * self.window[None, :, None]
+
+        # Overlap and Add
+        output_size = (T - 1) * self.hop_length + self.win_length
+        y = torch.nn.functional.fold(
+            ifft, output_size=(1, output_size), kernel_size=(1, self.win_length), stride=(1, self.hop_length),
+        )[:, 0, 0, pad:-pad]
+
+        # Window envelope
+        window_sq = self.window.square().expand(1, T, -1).transpose(1, 2)
+        window_envelope = torch.nn.functional.fold(
+            window_sq, output_size=(1, output_size), kernel_size=(1, self.win_length), stride=(1, self.hop_length),
+        ).squeeze()[pad:-pad]
+
+        # Normalize
+        assert (window_envelope > 1e-11).all()
+        y = y / window_envelope
+
+        return y
+
+
+
 @dataclass
 class MelSpectrogramConfig:
     sr: int = 22050
@@ -62,6 +134,13 @@ class MelSpectrogram(nn.Module):
         ).T
         self.mel_spectrogram.mel_scale.fb.copy_(torch.tensor(mel_basis))
 
+        self.istft = ISTFT(
+            n_fft=config.n_fft,
+            hop_length=config.hop_length,
+            win_length=config.win_length,
+            padding="same"
+        )
+
     def forward(self, audio: torch.Tensor) -> torch.Tensor:
         """
         :param audio: [B, T]
@@ -82,40 +161,21 @@ class MelSpectrogram(nn.Module):
 
         return mel
 
-    def inverse(self, spec: torch.Tensor, phase: torch.Tensor, n_fft: int) -> torch.Tensor:
+    def inverse(self, spec: torch.Tensor, phase: torch.Tensor) -> torch.Tensor:
         """
         Reconstruct waveform from magnitude+phase, undoing manual padding.
         :param spec: [B, n_fft//2+1, T]
         :param phase: [B, n_fft//2+1, T]
         :return: [B, T_reconstructed == original T]
         """
-        # Ensure the inputs are on the correct device.
-        spec = spec.to(self.device)
-        phase = phase.to(self.device)
 
-        # Combine magnitude and phase to create a complex spectrogram.
-        # Casting spec to complex if needed.
-        complex_spec = spec.to(torch.complex64) * torch.exp(1j * phase)
+        # 1) Определяем девайс и форматим комплекс-спектр
+        device = spec.device
+        # spec (float) -> complex, phase в exp(iφ)
+        complex_spec = spec.to(torch.complex64).to(device) * torch.exp(1j * phase.to(device))
 
-        # Create a Hann window. (win_length == n_fft у вас)
-        window = torch.hann_window(self.config.win_length, device=self.device)
-
-        # Inverse STFT without centering the signal.
-        # to match forward(..., center=False)
-        waveform = torch.istft(
-            complex_spec,
-            n_fft=n_fft,
-            hop_length=self.config.hop_length,
-            win_length=self.config.win_length,
-            window=window,
-            center=False,
-            normalized=False,
-            onesided=True
-        )
-
-        # Remove manual padding.
-        pad_amount = (self.config.n_fft - self.config.hop_length) // 2
-        if pad_amount > 0:
-            waveform = waveform[..., pad_amount:-pad_amount]
+        # 2) Прогоняем через custom ISTFT
+        #    ISTFT сам сделает irfft, overlap-add, нормализацию и "same"-трим
+        waveform = self.istft(complex_spec)
 
         return waveform
