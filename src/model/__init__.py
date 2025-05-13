191eeb41512dd1306d5808fbb148d51c995c18bc
Georgii Fatakhov <fatahoff.georgy@yandex.ru>
2025-05-13 20:50:36 +0300

    mambavoc_v3

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -4,11 +4,13 @@ from src.model.hifigan_paper import HiFiGANPaper
 from src.model.lightvoc import LightVoc
 from src.model.mambavoc import MambaVoc
 from src.model.mambavoc_v2 import MambaVocV2
+from src.model.mambavoc_v3 import MambaVocV3
 
 __all__ = [
     "HiFiGAN",
     "HiFiGANPaper",
     "LightVoc",
     "MambaVoc",
-    "MambaVocV2"
+    "MambaVocV2",
+    "MambaVocV3"
 ]
